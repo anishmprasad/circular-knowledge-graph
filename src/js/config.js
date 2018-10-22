b0f02bd85f3434801412fd486719cc935bc1eb7f
anish.m.prasad <anish.m.prasad@gmail.com>
2018-10-23 00:01:37 +0530

    core update

---
+++ src/js/config.js
@@ -0,0 +1,127 @@
+export const config = {
+  "API_BASE_URL": "https://opensourceprojects-pa.googleapis.com/v1/projects/",
+  "API_KEY": "AIzaSyC-j2zg47MN_K041U-KQwiQIIdPUZtvIkg",
+  "PROJECT_ID": "opensource-projectdb-prod",
+  "ENABLE_ERROR_REPORTING": true,
+  "LIST_PAGE_SIZE": 48,
+  "EXPLORE_PAGE_SIZE": 80,
+  "MOBILE_WIDTH": 720,
+  "COLORS": {
+    "GREEN": "#34A853",
+    "RED": "#EA4335",
+    "BLUE": "#4285F4",
+    "YELLOW": "#FBBC05"
+  },
+  "SORT_PARAMS": [
+    "Relevance",
+    "Name"
+  ],
+  "LANGUAGES": [
+    "c",
+    "c#",
+    "c++",
+    "css",
+    "dart",
+    "go",
+    "haskell",
+    "java",
+    "javascript",
+    "lua",
+    "objective-c",
+    "perl",
+    "php",
+    "python",
+    "r",
+    "ruby",
+    "rust",
+    "shell",
+    "swift",
+    "typescript"
+  ],
+  "CATEGORIES": [
+    {
+      "slug": "featured",
+      "cat": "Featured"
+    },
+    {
+      "slug": "cloud",
+      "cat": "Cloud"
+    },
+    {
+      "slug": "analytics-visualization",
+      "cat": "Data Analytics & Visualization"
+    },
+    {
+      "slug": "databases",
+      "cat": "Databases"
+    },
+    {
+      "slug": "developer-tools",
+      "cat": "Developer Tools"
+    },
+    {
+      "slug": "education",
+      "cat": "Education & Science"
+    },
+    {
+      "slug": "enterprise",
+      "cat": "Enterprise"
+    },
+    {
+      "slug": "games",
+      "cat": "Games & Entertainment"
+    },
+    {
+      "slug": "graphics-video-audio",
+      "cat": "Graphics, Video, & Audio"
+    },
+    {
+      "slug": "i18n",
+      "cat": "Internationalization & Localization"
+    },
+    {
+      "slug": "iot",
+      "cat": "Internet of Things"
+    },
+    {
+      "slug": "geo",
+      "cat": "Location & Maps"
+    },
+    {
+      "slug": "machine-learning",
+      "cat": "Machine Learning, Neural Networks, & AI"
+    },
+    {
+      "slug": "mobile",
+      "cat": "Mobile"
+    },
+    {
+      "slug": "networking",
+      "cat": "Networking"
+    },
+    {
+      "slug": "programming",
+      "cat": "Programming"
+    },
+    {
+      "slug": "samples",
+      "cat": "Samples & Examples"
+    },
+    {
+      "slug": "security",
+      "cat": "Security"
+    },
+    {
+      "slug": "testing",
+      "cat": "Testing"
+    },
+    {
+      "slug": "utilities",
+      "cat": "Utilities"
+    },
+    {
+      "slug": "web",
+      "cat": "Web"
+    }
+  ]
+}
---
+++ src/js/deeplink-service.js
@@ -0,0 +1,4 @@
+export const deeplinkService = {
+  query: null,
+  currentPage: "explore"
+}
